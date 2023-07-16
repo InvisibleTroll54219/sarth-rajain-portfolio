587918efa79a2e13130c66fa99c5144639b82310
Sarth Rajain <96329474+InvisibleTroll54219@users.noreply.github.com>
2023-07-17 00:12:45 +0530

    Add files via upload

---
+++ src/constants/index.js
@@ -0,0 +1,59 @@
+export const projects = [
+  {
+    title: "Virtual Mouse Mover",
+    desc: "A user interface that tracks user hand movement to move the mouse on-screen",
+    tags: [
+      {
+        name: "Python",
+        color: "red-600",
+      },
+    ],
+    
+    img: "https://drive.google.com/uc?export=view&id=1w7JuU1o68vZs0fEIRI4WFAc4uUkbf2_P",
+    code: "https://github.com/InvisibleTroll54219/virtual-mouse-mover",
+  },
+  {
+    title: "Web-Chat+Image-App",
+    desc: "An app made with PHP to help each other communicate fastly and securely. We can also search for the friends with their username.Realtime updates on messages and images posted.",
+    tags: [
+      {
+        name: "PHP",
+        color: "red-600",
+      },
+      {
+        name: "JavaScript",
+        color: "green-600",
+      },
+      {
+        name: "HTML/CSS",
+        color: "yellow-600",
+      },
+      {
+        name: "MySQL",
+        color: "blue-800",
+      },
+    ],
+    img: "https://drive.google.com/uc?export=view&id=17YYtszvHoye6pQimk8IUrX3jHVwVMbuE",
+    code: "https://github.com/InvisibleTroll54219/aeon-chat",
+  },
+  {
+    title: "Attendance-Tracker",
+    desc: "An attendance tracker app for classrooms that records the student attendance based on their geolocation.",
+    tags: [
+      {
+        name: "ReactNative",
+        color: "red-600",
+      },
+      {
+        name: "NodeJS",
+        color: "green-600",
+      },
+      {
+        name: "MongoDB",
+        color: "yellow-600",
+      },
+    ],
+    img: "https://drive.google.com/uc?export=view&id=1PpESeY4GChZZFlD1jssXrvwrFW-LHvNZ",
+    code: "https://github.com/InvisibleTroll54219/devhack_4.0",
+  },
+];
